0317d801f469f4c62fe436c949ac96a8e5130715
Karthik Ramesh <kathikramesh317@gmail.com>
2026-01-31 00:33:24 +0530

    remove unnecessary code

--- myApp.js
+++ myApp.js
@@ -2,21 +2,19 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-const timeinseconds = 90 *24 *60 *60;
-app.use(helmet.hsts({maxAge: timeinseconds, force: true}));
-app.use( helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "trusted-cdn.com"]
-  }
-}));
+
+app.use(helmet({
+ frameguard: {         //configure
+  action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives:{
+     defaultSrc: ["'self'"],
+     styleSrc: ["'self'", 'trust-cdn.com'],
+   }
+  },
+    nocache: true   // disable
+}))
 
 
 
